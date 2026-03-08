fb18c89e71f3961711ad92b46ad9296db94387db
ParsaMokhtarieh <Parsamok07@gmail.com>
2026-03-08 17:09:39 -0400

    Update script.js

--- lifeQuest/js/script.js
+++ lifeQuest/js/script.js
@@ -1,1 +1,17 @@
-
+window.onload = function() {
+    alert("Welcome to our website!");
+};
+function showDate() {
+    let today = new Date();
+    let date = today.toDateString();
+    document.getElementById("date").innerHTML = date;
+}
+function toggleMenu() {
+    let nav = document.getElementById("navMenu");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } 
+    else {
+        nav.style.display = "block";
+    }
+}
