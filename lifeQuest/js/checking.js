c77426f0f4529ea09e469bc71220005cc1d35f04
Cskirby34 <cmking3388@gmail.com>
2026-03-22 12:53:47 -0400

    Update checking.js

--- lifeQuest/js/checking.js
+++ lifeQuest/js/checking.js
@@ -1,3 +1,8 @@
 function checkForm(){
+  const userName = document.querySelector("");
+  const password = document.querySelector("");
+  const confirmPassword = document.querySelector("");
+
+  let errors = [];
   
 }
