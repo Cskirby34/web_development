f1f5fabf93cf061c3bf32ed92fb071ebdd8daa17
Cskirby34 <cmking3388@gmail.com>
2026-03-30 10:31:22 -0400

    Delete lifeQuest/js/checking.js

--- lifeQuest/js/checking.js
+++
@@ -1,8 +0,0 @@
-function checkForm(){
-  const userName = document.querySelector("");
-  const password = document.querySelector("");
-  const confirmPassword = document.querySelector("");
-
-  let errors = [];
-  
-}
