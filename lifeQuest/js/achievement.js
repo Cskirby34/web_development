a46933437bad939bb8e8a1cbd4dbcf4927bbbec2
Cskirby34 <cmking3388@gmail.com>
2026-03-31 10:03:02 -0400

    Create achievement.js

---
+++ lifeQuest/js/achievement.js
@@ -0,0 +1,30 @@
+const colors = [
+   "#ff3b3b", "#ffb703", "#4cc9f0",
+   "#80ed99", "#c77dff", "#f72585"
+];
+
+function createConfetti() {
+   const confetti = document.createElement("div");
+   confetti.classList.add("confetti");
+
+   const size = Math.random() * 8 + 6;
+   confetti.style.width = size + "px";
+   confetti.style.height = size * 1.4 + "px";
+   confetti.style.left = Math.random() * window.innerWidth + "px";
+
+   confetti.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+
+   const fallDuration = Math.random() * 3 + 2;
+
+   confetti.style.animationDuration =
+      `${fallDuration}s, ${Math.random() * 2 + 1}s`;
+
+   document.body.appendChild(confetti);
+
+   setTimeout(() => {
+      confetti.remove();
+   }, fallDuration * 1000);
+}
+
+setInterval(createConfetti, 100);
